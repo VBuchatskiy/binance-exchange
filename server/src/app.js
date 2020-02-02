43e477230b1edf159918a3c7864dc3918f2ee23a
Vladimir Buchatskiy <vladimirbuchatskiy@gmail.com>
2020-02-02 11:54:26 +0200

    calculate     rsi     macd     bollingerbands send     price     rsi     macd     bollingerbands

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import logger from 'morgan'
 import chalk from 'chalk'
 import parser from 'body-parser'
-import { exchange } from '@@/routers'
+import exchange from '@@/routers/exchange'
 import { port } from '@/config/config'
 
 const app = express();
@@ -12,8 +12,8 @@ app.use(logger('combined'))
 app.use(parser.json())
 app.use(cors())
 
-app.use('/exchange', exchange)
+app.use('/', exchange)
 
 app.listen(port, () => {
-  console.log(chalk.red(`http://localhost:${port}/`))
+  console.log(chalk.cyan(`http://localhost:${port}/`))
 });
