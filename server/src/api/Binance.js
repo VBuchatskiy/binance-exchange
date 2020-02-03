e277c7dbfd25f9b02d13fd5cb4b92857caf3166a
Vladimir Buchatskiy <vladimirbuchatskiy@gmail.com>
2020-02-04 00:35:23 +0200

    conflicts resolved

--- server/src/api/Binance.js
+++ server/src/api/Binance.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+import { BollingerBands, RSI, MACD, SMA } from '@@/indicators/TechnicalIndicators'
+import { klines, trades } from '@@/api/constants'
 //https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md
 const Binance = class {
-  constructor() {
-    this.host = 'https://api.binance.com'
+  constructor({ host = 'https://api.binance.com/api/v3/', query = '' } = {}) {
+    this.host = host
     this.path = {
       ping: '/api/v3/ping',
       time: '/api/v3/time',
@@ -11,6 +13,7 @@ const Binance = class {
       trades: '/api/v3/trades',
       klines: '/api/v3/klines'
     }
+    this.query = query
   }
   async klines({ symbol = 'BTCUSDT', interval = '4h', limit = 120, param } = {}) {
     const base = `${this.host}${this.path.klines}`
@@ -42,6 +45,45 @@ const Binance = class {
       }
     } catch (error) {
       console.error(error.message)
+    }
+  }
+  async indicators({ symbol = 'BTCUSDT', interval = '4h', limit = 120 } = {}) {
+    const price = await this.trades({ symbol, param: trades[2] })
+    const prices = await this.klines({ symbol, interval, limit, param: klines['CLOSE_PRICE'] })
+    const ma = {
+      fast: new SMA({
+        values: prices,
+        period: 12
+      }).pop(),
+      slow: new SMA({
+        values: prices,
+        period: 26
+      }).pop()
+    }
+    const rsi = new RSI({
+      values: prices,
+      period: 14
+    }).pop()
+    const bollingerbands = new BollingerBands({
+      values: prices,
+      period: 20,
+      stdDev: 2
+    }).pop()
+    const macd = new MACD({
+      values: prices,
+      fastPeriod: 12,
+      slowPeriod: 26,
+      signalPeriod: 9,
+      SimpleMAOscillator: false,
+      SimpleMASignal: false
+    }).pop()
+
+    return {
+      price,
+      ma,
+      rsi,
+      bollingerbands,
+      macd
     }
   }
 }
