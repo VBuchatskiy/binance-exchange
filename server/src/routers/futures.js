41d8f99962398e3fd2612fd645d98c1420be5be4
Vladimir Buchatskiy <vladimirbuchatskiy@gmail.com>
2020-02-04 22:55:44 +0200

    query req settings

--- server/src/routers/futures.js
+++ server/src/routers/futures.js
@@ -5,14 +5,25 @@ import { endpoints } from '@@/api/constants'
 const futures = new Router()
 
 futures.get('/', (req, res) => {
-  new Binance(endpoints[1])
-    .indicators()
-    .then(response => {
-      res.json(response)
-    })
-    .catch(error => {
-      console.warn(error.message)
-    })
+  if (!Object.keys(req.query).length) {
+    new Binance({ host: endpoints[2] })
+      .indicators()
+      .then(response => {
+        res.json(response)
+      })
+      .catch(error => {
+        console.warn(error.message)
+      })
+  } else {
+    new Binance({ host: endpoints[2] })
+      .indicators({ symbol: req.query.symbol, interval: req.query.interval })
+      .then(response => {
+        res.json(response)
+      })
+      .catch(error => {
+        console.warn(error.message)
+      })
+  }
 })
 
 export default futures
